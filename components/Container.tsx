c0c3893ff30d7880ffb39428d6486cdf8c19ea76
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-10-26 16:52:08 -0700

    add container for the layout

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,11 @@
+import Nav from './Nav';
+
 export default function Container({ children }: { children: JSX.Element[] }) {
-  return <div className="mx-auto w-[88%] max-w-5xl py-4">{children}</div>;
+  return (
+    <div className="mx-auto w-[88%] max-w-5xl py-4">
+      <Nav />
+
+      {children}
+    </div>
+  );
 }
