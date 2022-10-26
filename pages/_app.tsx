fd01ff0b595d5b8128698860f4ff5d2025e3a0b4
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-10-26 13:49:24 -0700

    add theme provider to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+// Contexts
+import { ThemeProvider } from 'next-themes';
+import { ThemeToggleProvider } from '../contexts/ThemeContext';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      enableSystem
+      enableColorScheme
+      defaultTheme="system"
+      attribute="class"
+    >
+      <ThemeToggleProvider>
+        <Component {...pageProps} />
+      </ThemeToggleProvider>
+    </ThemeProvider>
+  );
 }
