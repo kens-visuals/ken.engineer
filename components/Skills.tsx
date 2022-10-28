537a1f5f076b945d94a78f32a4dcfbcec6d91dcb
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-10-27 17:12:14 -0700

    fix background blur and mix-blend of texts

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -10,40 +10,40 @@ export default function Skills() {
       </h2>
 
       <div className="my-10 grid grid-cols-2 grid-rows-mobile-row-layout gap-4 text-3xl font-bold md:grid-cols-4 md:grid-rows-tablet-row-layout md:text-4xl lg:grid-flow-row-dense lg:grid-cols-6 lg:grid-rows-desk-row-layout">
-        <div className="relative col-span-2 flex items-start justify-end rounded-lg bg-main-tools-pattern p-6 ">
+        <div className="relative col-span-2 flex items-start justify-end rounded-lg bg-main-tools-pattern  p-6">
           <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg bg-other-tools-pattern p-6 backdrop-blur-sm ">
-            <span className="mix-blend-overlay">Main Tools</span>
+          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg p-6 mix-blend-overlay backdrop-blur-sm ">
+            Main Tools
           </div>
         </div>
         <div className="relative col-span-2 flex items-start justify-end rounded-lg bg-learning-tools-pattern p-6 md:row-start-3 md:row-end-4 lg:row-start-2 lg:row-end-3">
           <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg bg-other-tools-pattern p-6 backdrop-blur-sm ">
-            <span className="mix-blend-overlay">Currently Learning</span>
+          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg p-6 mix-blend-overlay backdrop-blur-sm ">
+            Currently Learning
           </div>
         </div>
         <div className="relative flex items-start justify-end rounded-lg bg-uiux-tools-pattern p-6">
           <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg bg-other-tools-pattern p-6 backdrop-blur-sm ">
-            <span className="mix-blend-overlay">UI/UX</span>
+          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg p-6 mix-blend-overlay backdrop-blur-sm ">
+            UI/UX
           </div>
         </div>
         <div className="relative flex items-start justify-end rounded-lg bg-apps-pattern p-6">
           <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg bg-other-tools-pattern p-6 backdrop-blur-sm ">
-            <span className="mix-blend-overlay">Apps</span>
+          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg p-6 mix-blend-overlay backdrop-blur-sm ">
+            Apps
           </div>
         </div>
         <div className="relative col-span-2 flex items-start justify-end rounded-lg bg-resume-pattern p-6">
           <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg bg-other-tools-pattern p-6 backdrop-blur-sm ">
-            <span className="mix-blend-overlay">Resume</span>
+          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg  p-6 mix-blend-overlay backdrop-blur-sm ">
+            Resume
           </div>
         </div>
-        <div className="relative col-span-2 row-span-2 flex items-start justify-end rounded-lg p-6 lg:col-start-5 lg:col-end-7 lg:row-start-1 lg:row-end-3">
+        <div className="relative col-span-2 row-span-2 flex items-start justify-end rounded-lg bg-other-tools-pattern p-6 lg:col-start-5 lg:col-end-7 lg:row-start-1 lg:row-end-3">
           <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg bg-other-tools-pattern p-6 backdrop-blur-sm ">
-            <span className="mix-blend-overlay">Other Tools</span>
+          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg  p-6 mix-blend-overlay backdrop-blur-sm ">
+            Other Tools
           </div>
         </div>
       </div>
