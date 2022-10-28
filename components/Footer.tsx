ddc519c5151490905273b972a9c5bf875d120dc5
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-10-28 12:59:50 -0700

    add links to footer section and small style changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,66 @@
 export default function Footer() {
   return (
-    <footer>
-      <span>Designed and Coded by </span>
-      <a
-        href="https://github.com/kens-visuals"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Ken Nersisyan
-      </a>
+    <footer className="border-t border-t-primary-dark pt-6 dark:border-t-primary-light">
+      <div className="my-8 grid gap-4 md:grid-cols-3 lg:max-w-3xl">
+        <ul className="space-y-2">
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Skills</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Articles</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+
+        <ul className="space-y-2">
+          <li>
+            <a href="#">GitHub</a>
+          </li>
+          <li>
+            <a href="#">LinkedIn</a>
+          </li>
+          <li>
+            <a href="#">Medium</a>
+          </li>
+          <li>
+            <a href="#">Twitter</a>
+          </li>
+          <li>
+            <a href="#">Discord</a>
+          </li>
+        </ul>
+
+        <ul className="space-y-2">
+          <li>
+            <a href="#">Books</a>
+          </li>
+          <li>
+            <a href="#">Design</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex items-center justify-center">
+        <span>
+          Designed and Coded by{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/kens-visuals"
+            className="text-js-yellow underline underline-offset-4"
+          >
+            Ken Nersisyan
+          </a>
+        </span>
+      </div>
     </footer>
   );
 }
