833fd7494fb8793fa9a5edc0514ab1d55e058459
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-10-29 16:48:51 -0700

    change bg and fix alignment

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,7 +7,7 @@ import ThemeToggler from './ThemeToggler';
 export default function Nav() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
-  const menuItems = ['Skills', 'Projects', 'Articles', 'Contact', 'Books'];
+  const menuItems = ['Skills', 'Projects', 'Articles', 'Contact', 'Resume'];
 
   const menuItemsDisplay = menuItems.map((item) => (
     <li key={item} className="flex items-center">
@@ -18,9 +18,12 @@ export default function Nav() {
   ));
 
   return (
-    <nav className="fixed top-0 left-1/2 z-50 block w-[88%] max-w-5xl -translate-x-1/2 rounded-b-lg border-b border-primary-dark bg-nav-pattern pb-4 pt-6 backdrop-blur-lg dark:border-primary-light">
-      <div className="relative flex gap-4 font-inter md:static md:items-center ">
-        <Link className="mr-auto text-body uppercase" href="/">
+    <nav className="fixed top-0 left-1/2 z-50 w-[88%] max-w-5xl -translate-x-1/2 rounded-b-lg bg-primary-light/70 pb-4 pt-6 backdrop-blur-lg dark:bg-primary-dark/30">
+      <div className="relative flex gap-4 px-6 font-inter md:static md:items-center">
+        <Link
+          className="mr-auto text-body uppercase text-primary-dark dark:text-primary-light"
+          href="/"
+        >
           Ken.Engineer
         </Link>
 
@@ -69,7 +72,7 @@ export default function Nav() {
 
         {/* Mobile Menu */}
         {isMenuOpen && (
-          <ul className="absolute top-14 right-0 z-10 flex w-full max-w-sm flex-col gap-4 rounded-md bg-nav-pattern p-4 text-primary-dark backdrop-blur-lg dark:bg-nav-pattern  dark:text-primary-light md:hidden">
+          <ul className="absolute top-14 right-0 z-10 flex w-full max-w-sm flex-col gap-4 rounded-md bg-primary-light/70 p-4 text-primary-dark backdrop-blur-lg dark:bg-primary-dark/30 dark:text-primary-light md:hidden">
             {menuItemsDisplay}
             <li className="flex items-center">
               <ThemeToggler />
@@ -78,7 +81,7 @@ export default function Nav() {
         )}
 
         {/* Tablet and Desktop menu */}
-        <ul className="hidden w-full gap-4 p-4 text-primary-dark dark:text-primary-light md:flex md:justify-end md:text-primary-dark">
+        <ul className="hidden w-full gap-4 p-4 pr-0 text-primary-dark dark:text-primary-light md:flex md:justify-end">
           {menuItemsDisplay}
           <li className="flex items-center">
             <ThemeToggler />
