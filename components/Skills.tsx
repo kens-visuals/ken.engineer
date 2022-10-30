dfe43290a62a10a962426c886da79d1594e34c82
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-10-29 17:12:50 -0700

    refactor Skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,6 +1,8 @@
-import Image from 'next/image';
+// Components
+import SkillsLogosList from './SkillsLogosList';
+
 // Assets
-import shape from '../public/assets/shapes.svg';
+import * as icons from '../utils/skillsLogos';
 
 export default function Skills() {
   return (
@@ -10,50 +12,58 @@ export default function Skills() {
       </h2>
 
       <div className="my-10 grid grid-cols-2 grid-rows-mobile-row-layout gap-4 text-3xl font-bold md:grid-cols-4 md:grid-rows-tablet-row-layout md:text-4xl lg:grid-flow-row-dense lg:grid-cols-6 lg:grid-rows-desk-row-layout">
-        <div className="relative col-span-2 flex items-start justify-end rounded-lg bg-main-tools-pattern  p-6">
-          <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg p-6 backdrop-blur-sm ">
-            <span className="bg-main-tools-pattern--text bg-clip-text text-transparent">
+        <div className="relative col-span-2 flex items-start rounded-lg bg-main-tools-pattern p-4 md:row-span-2 lg:col-start-1 lg:col-end-3 lg:row-start-1 ">
+          <ul className="flex w-full flex-wrap items-center gap-1">
+            <SkillsLogosList logosArr={icons.mainToolsLogos} />
+          </ul>
+
+          {/* NOTE: background blur is set manually, change if needed */}
+          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg backdrop-blur-[7px]">
+            <span className="bg-main-tools-pattern--text bg-clip-text p-4 text-transparent">
               Main Tools
             </span>
           </div>
         </div>
-        <div className="relative col-span-2 flex items-start justify-end rounded-lg bg-learning-tools-pattern p-6 md:row-start-3 md:row-end-4 lg:row-start-2 lg:row-end-3">
-          <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg p-6 backdrop-blur-sm ">
-            <span className="bg-learning-tools-pattern--text bg-clip-text text-transparent">
+        <div className="relative col-span-2 flex items-start rounded-lg bg-learning-tools-pattern p-4 md:row-start-3 md:row-end-4 lg:row-start-2 lg:row-end-3">
+          <ul className="flex flex-wrap items-center gap-1">
+            <SkillsLogosList logosArr={icons.currentlyLearningLogos} />
+          </ul>
+
+          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg backdrop-blur-sm ">
+            <span className="bg-learning-tools-pattern--text bg-clip-text p-4 text-transparent">
               Currently Learning
             </span>
           </div>
         </div>
-        <div className="relative flex items-start justify-end rounded-lg bg-uiux-tools-pattern p-6">
-          <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg p-6 backdrop-blur-sm ">
-            <span className="bg-uiux-tools-pattern--text bg-clip-text text-transparent">
+        <div className="relative flex items-start rounded-lg bg-uiux-tools-pattern p-4">
+          <ul className="flex flex-wrap items-center gap-1">
+            <SkillsLogosList logosArr={icons.uiuxLogos} />
+          </ul>
+
+          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg backdrop-blur-sm ">
+            <span className="bg-uiux-tools-pattern--text bg-clip-text p-4 text-transparent">
               UI/UX
             </span>
           </div>
         </div>
-        <div className="relative flex items-start justify-end rounded-lg bg-apps-pattern p-6">
-          <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg p-6 backdrop-blur-sm ">
-            <span className="bg-apps-pattern--text bg-clip-text text-transparent">
+        <div className="relative flex items-start rounded-lg bg-apps-pattern p-4">
+          <ul className="flex flex-wrap items-center gap-1">
+            <SkillsLogosList logosArr={icons.appsLogos} />
+          </ul>
+
+          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg backdrop-blur-sm ">
+            <span className="bg-apps-pattern--text bg-clip-text p-4 text-transparent">
               Apps
             </span>
           </div>
         </div>
-        <div className="relative col-span-2 flex items-start justify-end rounded-lg bg-resume-pattern p-6">
-          <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg  p-6 backdrop-blur-sm ">
-            <span className="bg-resume-pattern--text bg-clip-text text-transparent">
-              Resume
-            </span>
-          </div>
-        </div>
-        <div className="relative col-span-2 row-span-2 flex items-start justify-end rounded-lg bg-other-tools-pattern p-6 lg:col-start-5 lg:col-end-7 lg:row-start-1 lg:row-end-3">
-          <Image src={shape} alt="" height={100} width={50} />
-          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg  p-6 backdrop-blur-sm ">
-            <span className="bg-other-tools-pattern--text bg-clip-text text-transparent">
+        <div className="relative col-span-2 row-span-2 flex items-start rounded-lg bg-resume-pattern p-4 lg:col-start-5 lg:col-end-7 lg:row-start-1 lg:row-end-3">
+          <ul className="flex flex-wrap items-center gap-1">
+            <SkillsLogosList logosArr={icons.otherToolsLogos} />
+          </ul>
+
+          <div className="absolute inset-0 z-10 flex h-full w-full items-end rounded-lg backdrop-blur-sm">
+            <span className="bg-resume-pattern--text bg-clip-text p-4 text-transparent">
               Other Tools
             </span>
           </div>
