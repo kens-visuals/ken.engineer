cd6fbb1bfd14280f155a4afcfe0dc14e7369191e
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2026-03-15 16:26:51 -0700

    fix: Container children type to accept single child (ReactNode)

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,7 +1,8 @@
+import { ReactNode } from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
 
-export default function Container({ children }: { children: JSX.Element[] }) {
+export default function Container({ children }: { children: ReactNode }) {
   return (
     <div className="mx-auto w-[92%] max-w-5xl overflow-x-hidden py-4 lg:overflow-x-visible">
       <Nav />
